a961ae300a0f9383d81012f2a8198ddc69700dc8
Maximilian Walterskirchen <me@mwalterskirchen.dev>
2025-12-20 13:56:48 +0100

    Enhance Angular server-side rendering setup and update dependencies
    - Added `main.server.ts` for server-side bootstrapping of the Angular application.
    - Introduced `app.config.server.ts` to merge server-specific configurations.
    - Updated `angular.json` to specify server entry point and output mode.
    - Added `@angular/platform-server` and `@angular/ssr` dependencies in `package.json` and `package-lock.json`.
    - Cleaned up `styles.css` by removing unused styles and comments.
    - Updated `app.config.ts` to remove unnecessary error listener provider.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,14 @@
-import { ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     {
       provide: DEFAULT_CURRENCY_CODE,
-      useValue: 'CHF'
+      useValue: 'CHF',
     },
-  ]
+  ],
 };
